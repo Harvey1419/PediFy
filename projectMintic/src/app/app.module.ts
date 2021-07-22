a205c13ae194bd3772f5a26e3cbfc335bbc41c6a
Harvey Quintero <juhquint@bancolombia.com.co>
2021-07-21 23:17:57 -0500

    Se agrego pequeña estructura limpia al programa, se instaló angular material y se creo el boceto de la landing page

--- projectMintic/src/app/app.module.ts
+++ projectMintic/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './UI/landing-page/landing-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
